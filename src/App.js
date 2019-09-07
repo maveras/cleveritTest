50207286e25ebe29052a7a2a1de207434984314a
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2019-09-07 00:22:44 -0300

    added reset css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
+import ".";
 import { UserContainer } from "./containers/UserContainer";
 
 export const App = () => (
-  <div className="App">
+  <div className="App Reset">
     <UserContainer></UserContainer>
   </div>
 );
